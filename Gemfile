c7d32bb4e5f04014128658b9a39ef2039b551d36
Jennifer Wu <jenniferwu0621@berkeley.edu>
2015-02-02 19:05:07 -0400

    added rerun and test changing msg

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 
 gem 'sinatra', '>= 1.4'
+
+group :development do
+  gem 'rerun'
+end
--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+    "<!DOCTYPE html><html><head></head><body><h1>Goodbye World</h1><h2>New Message</h2></body></html>"
   end
 end
