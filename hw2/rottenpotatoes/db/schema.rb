f0b158b955d6a5e7e0f625b2576c54b9310ddf22
Jennifer Wu <jenniferwu0621@berkeley.edu>
2015-02-10 17:13:50 -0400

    migrated db again, first deployment to heroku x2

--- hw2/rottenpotatoes/db/schema.rb
+++ hw2/rottenpotatoes/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111119180638) do
+ActiveRecord::Schema.define(:version => 20150210211256) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
