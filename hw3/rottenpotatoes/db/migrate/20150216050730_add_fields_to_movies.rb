c7d7af525547c439178a44784ebcd8327b8fd19d
Jennifer Wu <jenniferwu0621@berkeley.edu>
2015-03-15 23:59:02 -0700

    merged hw3 into rottenpotatoes

--- hw3/rottenpotatoes/db/migrate/20150216050730_add_fields_to_movies.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToMovies < ActiveRecord::Migration
-  def change
-    create_table :movies
-    add_column :movies, :title, :string
-    add_column :movies, :release_date, :datetime
-    add_column :movies, :rating, :string
-    add_column :movies, :description, :text
-  end
-end
